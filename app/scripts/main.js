74a27ddf34f79f60294e9fd5934f137846fd7ca3
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-11-19 08:29:16 +0100

    Event Delegation example finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-  $("#list a").on( "click", function(event){
+  //We create a delegated event!
+  $( "#list" ).on( "click", "a", function( event ) {
     event.preventDefault();
-    console.log( $( this ).text() );
+    console.log($(this).text());
   });
-
-  //We add a new element to the list after binding event.
-  // Add a new element on to our existing list
-  $( "#list" ).append( "<li><a href='http://newdomain.com'>Item #5</a></li>" );
 });
 
