3d419b36781259cbbea4bf4851bd66a00f79ac23
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-11-19 08:17:37 +0100

    Usually jquery event handling built

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,7 @@
-(function () {
-  // Get the parent DIV, add click listener...
-  document.getElementById("myDiv").addEventListener("click",function(e) {
-    // e.target was the clicked element
-    if (e.target && e.target.matches("a.classA")) {
-      console.log("Anchor element clicked!");
-    }
+$(document).ready(function() {
+  $("#list a").on( "click", function(event){
+    event.preventDefault();
+    console.log( $( this ).text() );
   });
-})();
+});
+
