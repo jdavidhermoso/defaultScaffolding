5d7e6f407961efc64acc368df766965105ee9450
Juan David Hermoso <juandavidhermoso@gmail.com>
2016-11-19 08:20:38 +0100

    Event are not firing When last element is clicked, due to We perform the jquery .on method before it existed on the DOM

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
     console.log( $( this ).text() );
   });
+
+  //We add a new element to the list after binding event.
+  // Add a new element on to our existing list
+  $( "#list" ).append( "<li><a href='http://newdomain.com'>Item #5</a></li>" );
 });
 
